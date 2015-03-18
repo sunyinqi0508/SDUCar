c28859c4e56f37d12d4c7d3c96175a609d0c4d1e
YuYu <wangzhenyujiang@163.com>
2015-03-18 20:57:42 +0800

    First Commit

---
+++ SDUCar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SDUCar
+//
+//  Created by 王振宇 on 15/3/18.
+//  Copyright (c) 2015年 王振宇. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
